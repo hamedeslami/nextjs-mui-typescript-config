1cd2c5abe9d273bd41d8084ba39616637f4064f4
Hamed Eslami <h.eslami@behsacorp.com>
2025-04-08 11:40:41 +0330

    add not found page

---
+++ src/constant/not-found.ts
@@ -0,0 +1,6 @@
+const NotFoundMessage: { [key: string]: string } = Object.freeze({
+    TITLE: "صفحه‌ای که دنبال آن بودید پیدا نشد!",
+    HOME_BUTTON: "بازگشت به صفحه اصلی"
+});
+
+export default NotFoundMessage;
