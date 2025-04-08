d73c1a1cd750743cf6d2b64184674bcbe4af910b
Hamed Eslami <h.eslami@behsacorp.com>
2025-04-08 14:32:23 +0330

    refactor not found component

--- src/components/UI/NotFound/index.tsx
+++ src/components/UI/NotFound/index.tsx
@@ -4,12 +4,12 @@ import NotFoundMessage from "@/constant/not-found";
 import { Button, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 
-export default function NotFound(){
-    return(
+export default function NotFound() {
+    return (
         <Stack flexDirection={"column"} justifyContent={"center"} alignItems={"center"} height={"100vh"} gap={2}>
-        <Image src="/assets/images/vectors/not-found.svg" alt="not found vector" width={550} height={450}/>
-        <Typography variant={"h5"} component={"h1"} fontWeight={'bold'}>{NotFoundMessage.TITLE}</Typography>
-        <Button variant="contained">{NotFoundMessage.HOME_BUTTON}</Button>
-      </Stack>
+            <Image src="/assets/images/vectors/not-found.svg" alt="not found vector" width={550} height={450} />
+            <Typography variant={"h5"} component={"h1"} fontWeight={'bold'}>{NotFoundMessage.TITLE}</Typography>
+            <Button variant="contained">{NotFoundMessage.HOME_BUTTON}</Button>
+        </Stack>
     )
 }
